57b78f6ea9f32569cac8cb3084abc75a4af88b74
SenEvgYur <es@kraski.ru>
2022-06-14 13:42:31 +0300

    написан код решения задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите массив, используя в качестве разделителя пробел или запятую: ");
+string [] massive = Console.ReadLine().Split(' ', ',');
+Console.Write("[ ");
+for (int i = 0; i < massive.Length; i++)
+{
+    if(massive[i].Length >=3)
+    {
+       Console.Write(massive[i] + " ");
+    }
+}
+Console.Write("]");
